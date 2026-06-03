34a8388a8da91f2729c87995933595bbfb2c6cce
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-06-03 15:47:22 +0700

    feat: add FloatingNav component for improved navigation experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/features/home/components/Hero";
+import { FloatingNav } from "@/shared/components/FloatingNavbar";
+import { Home } from "lucide-react";
 
 const page = () => {
   return (
     <main className="bg-black-100 relative flex flex-col items-center justify-center overflow-hidden">
       <div className="w-full">
+        <FloatingNav navItems={[{ name: "Home", link: "/", icon: <Home /> }]} />
         <Hero />
       </div>
     </main>

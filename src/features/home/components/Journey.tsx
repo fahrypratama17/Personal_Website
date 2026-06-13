20805e566a2dc9513592afd9f68ef4bd37616f61
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-06-13 22:54:48 +0700

    feat: enhance layout and styling in CardReveal, Journey, and Reality components for improved responsiveness and readability

--- src/features/home/components/Journey.tsx
+++ src/features/home/components/Journey.tsx
@@ -7,17 +7,23 @@ const Journey = () => {
       <h1 className="font-heading heading text-6xl text-white">
         My Career <span className="text-violet-300">Journey</span>
       </h1>
-      <div className="mx-auto mt-20 grid w-[85%] grid-cols-2 items-center justify-center gap-x-20 gap-y-12 py-12">
+      <div className="mx-auto mt-8 grid w-[90%] grid-cols-1 items-center justify-center gap-x-20 gap-y-6 py-12 md:mt-20 md:w-[85%] md:grid-cols-2 md:gap-y-12">
         {workExperience.map(({ id, title, desc, thumbnail }) => (
           <Button
-            className="font-body grid w-full grid-cols-[0.3fr_1fr] gap-4 p-4"
+            className="font-body flex w-full flex-col gap-4 p-4 md:grid md:grid-cols-[0.3fr_1fr]"
             key={id}
           >
-            <img src={thumbnail} alt={title} className="h-30 w-30" />
-            <div className="flex flex-col gap-4 text-start">
-              <h3 className="font-heading text-3xl font-bold">{title}</h3>
+            <img
+              src={thumbnail}
+              alt={title}
+              className="h-20 w-20 md:h-30 md:w-30"
+            />
+            <div className="flex flex-col gap-4 text-center md:text-start">
+              <h3 className="font-heading text-xl font-bold md:text-3xl">
+                {title}
+              </h3>
 
-              <p className="font-heading">{desc}</p>
+              <p className="font-heading text-sm">{desc}</p>
             </div>
           </Button>
         ))}

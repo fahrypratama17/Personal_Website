6264e99b58d5328d2ea4118bb7510c282dc60fbe
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-06-13 21:52:20 +0700

    feat: enhance layout and styling in BackgroundGradient, Grid, and GridGlobe components for improved responsiveness and readability

--- src/features/home/components/Grid.tsx
+++ src/features/home/components/Grid.tsx
@@ -9,23 +9,29 @@ import { GridGlobe } from "@/shared/components/GridGlobe";
 export default function Grid() {
   return (
     <section className="section">
-      <BentoGrid className="mx-auto mb-4 w-[85%] md:grid-cols-[1fr_0.7fr]">
+      <BentoGrid className="mx-auto mb-4 flex w-[90%] flex-col md:grid md:w-[85%] md:grid-cols-[1fr_0.7fr]">
         {items.slice(0, 3).map((item, i) => (
           <BentoGridItem
             key={i}
             title={item.title}
             header={item.header}
-            className={cn(i === 0 ? "md:row-span-2" : "", "font-body")}
+            className={cn(
+              i === 0 ? "md:row-span-2" : "",
+              "font-body overflow-hidden",
+            )}
           />
         ))}
       </BentoGrid>
-      <BentoGrid className="mx-auto w-[85%] md:grid-cols-[0.7fr_1fr]">
+      <BentoGrid className="mx-auto flex w-[90%] flex-col md:grid md:w-[85%] md:grid-cols-[0.7fr_1fr]">
         {items.slice(3, 6).map((item, i) => (
           <BentoGridItem
             key={i}
             title={item.title}
             header={item.header}
-            className={cn(i === 1 ? "md:row-span-2" : "", "font-body")}
+            className={cn(
+              i === 1 ? "md:row-span-2" : "",
+              "font-body overflow-hidden",
+            )}
           />
         ))}
       </BentoGrid>
@@ -36,7 +42,7 @@ export default function Grid() {
 const items = [
   {
     title: (
-      <p className="bg-black-100/40 hover:bg-black-100 absolute bottom-0 left-0 m-8 w-[80%] rounded-2xl p-4 text-4xl backdrop-blur-sm transition-colors duration-300">
+      <p className="bg-black-100/40 hover:bg-black-100 absolute bottom-0 left-0 m-4 w-[80%] rounded-2xl p-2 text-sm backdrop-blur-sm transition-colors duration-300 md:m-8 md:p-4 md:text-4xl">
         Transforming ideas into impactful solutions.
       </p>
     ),
@@ -58,10 +64,10 @@ const items = [
       <div className="flex flex-col justify-center gap-4 rounded-2xl p-4">
         <div>
           <p className="text-sm tracking-[0.3em] uppercase">Always evolving</p>
-          <p className="text-2xl">and refining my craft.</p>
+          <p className="text-xl md:text-2xl">and refining my craft.</p>
         </div>
 
-        <div className="flex w-full flex-col gap-4 text-center text-lg">
+        <div className="flex w-full flex-col gap-4 text-center text-sm md:text-lg">
           <div className="flex gap-4">
             {["Java", "JavaScript", "TypeScript", "Python"].map((item) => (
               <p
@@ -89,19 +95,21 @@ const items = [
   },
   {
     title: (
-      <div className="absolute bottom-8 left-12 mx-auto flex w-[80%] flex-col items-center justify-center gap-4 overflow-hidden p-4 text-center text-2xl">
+      <div className="absolute bottom-0 left-6 mx-auto flex h-50 w-[90%] flex-col items-center justify-center gap-4 overflow-hidden p-4 text-center text-lg md:bottom-8 md:left-12 md:h-auto md:w-[80%] md:text-2xl">
         <p>Developer at heart with a passion for technology.</p>
-        <p className="text-lg">
+        <p className="text-sm md:text-lg">
           Bridging the gap between clean code and intuitive design.
         </p>
       </div>
     ),
-    header: <div></div>,
+    header: <div className="h-50"></div>,
   },
   {
     title: (
-      <div className="bg-black-100/40 hover:bg-black-100 absolute bottom-0 left-0 m-8 w-[80%] rounded-2xl p-4 text-4xl backdrop-blur-sm transition-colors duration-300">
-        <p className="mb-4 text-lg tracking-[0.3em] uppercase">Current Focus</p>
+      <div className="bg-black-100/40 hover:bg-black-100 absolute bottom-0 left-0 m-4 w-[80%] rounded-2xl p-2 text-sm backdrop-blur-sm transition-colors duration-300 md:m-8 md:p-4 md:text-4xl">
+        <p className="mb-2 text-sm tracking-[0.3em] uppercase md:mb-4 md:text-lg">
+          Current Focus
+        </p>
         <p>Developing a modern platform for faculty events.</p>
       </div>
     ),
@@ -117,7 +125,7 @@ const items = [
   },
   {
     title: (
-      <div className="absolute bottom-8 left-12 flex w-[80%] flex-col items-center justify-center gap-4 rounded-2xl p-4 text-center text-2xl backdrop-blur-sm transition-colors duration-300">
+      <div className="absolute bottom-4 left-4 flex w-[90%] flex-col items-center justify-center gap-4 rounded-2xl p-4 text-center text-xl backdrop-blur-sm transition-colors duration-300 md:bottom-8 md:left-12 md:w-[80%] md:text-2xl">
         <p>Have a project in mind? Let's make it happen.</p>
         <MagicButton
           title="Copy my email address"
